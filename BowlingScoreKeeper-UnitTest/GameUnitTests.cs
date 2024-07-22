aa281633855ae5eee87d2328180cad1c7017b63b
TyShelbyCrenshaw <linxtree@gmail.com>
2024-07-21 23:26:19 -0600

    Add project files.

---
+++ BowlingScoreKeeper-UnitTest/GameUnitTests.cs
@@ -0,0 +1,284 @@
+using BowlingScoreKeeper.Components.Pages;
+
+namespace BowlingScoreKeeper_UnitTest
+{
+	[TestFixture]
+	public class Tests
+	{
+		public Game game;
+
+		[SetUp]
+		public void Setup()
+		{
+			game = new Game();
+		}
+
+		[Test]
+		public void TestThatThereAre10Frames()
+		{
+			Assert.AreEqual(10, game.Frames.Count);
+		}
+
+
+		[Test]
+		public void TestInitialScore()
+		{
+			Assert.AreEqual(null, game.Frames[0].Score);
+		}
+
+		[Test]
+		public void TestAddingSingleRoll()
+		{
+			game.AddRoll(5);
+			Assert.AreEqual(5, game.CurrentFrame.FirstRoll);
+		}
+
+		[Test]
+		public void TestCompletingFrame()
+		{
+			game.AddRoll(5);
+			game.AddRoll(3);
+			Assert.AreEqual(1, game.CurrentFrameIndex);
+			Assert.AreEqual(8, game.Frames[0].Score);
+		}
+
+		[Test]
+		public void TestStrike()
+		{
+			game.AddRoll(10);
+			Assert.AreEqual(1, game.CurrentFrameIndex);
+			Assert.IsNull(game.Frames[0].Score); // Score can't be calculated yet
+		}
+
+		[Test]
+		public void TestSpare()
+		{
+			game.AddRoll(5);
+			game.AddRoll(5);
+			Assert.AreEqual(1, game.CurrentFrameIndex);
+			Assert.Null(game.Frames[0].Score); // Score can't be calculated yet
+		}
+
+		[Test]
+		public void TestStrikeFollowedByRegularFrame()
+		{
+			game.AddRoll(10); // Frame 1
+			game.AddRoll(3);  // Frame 2
+			game.AddRoll(4);  // Frame 2
+			Assert.AreEqual(2, game.CurrentFrameIndex);
+			Assert.AreEqual(17, game.Frames[0].Score);
+			Assert.AreEqual(24, game.Frames[1].Score);
+		}
+
+		[Test]
+		public void TestSpareFollowedByRegularRoll()
+		{
+			game.AddRoll(5);  // Frame 1
+			game.AddRoll(5);  // Frame 1
+			game.AddRoll(3);  // Frame 2
+			Assert.AreEqual(1, game.CurrentFrameIndex);
+			Assert.AreEqual(13, game.Frames[0].Score);
+		}
+
+		[Test]
+		public void TestPerfectGame()
+		{
+			for (int i = 0; i < 12; i++)
+			{
+				game.AddRoll(10);
+			}
+			Assert.AreEqual(300, game.Frames[9].Score);
+		}
+
+		[Test]
+		public void TestWorstGame()
+		{
+			for (int i = 0; i < 20; i++)
+			{
+				game.AddRoll(0);
+			}
+			Assert.AreEqual(0, game.Frames[9].Score);
+		}
+
+		[Test]
+		public void TestTenthFrameStrike()
+		{
+			for (int i = 0; i < 18; i++)
+			{
+				game.AddRoll(0);
+			}
+			game.AddRoll(10); // 10th frame, first roll
+			game.AddRoll(5);  // 10th frame, second roll
+			game.AddRoll(3);  // 10th frame, third roll
+			Assert.AreEqual(18, game.Frames[9].Score);
+		}
+
+		[Test]
+		public void TestTenthFrameSpare()
+		{
+			for (int i = 0; i < 18; i++)
+			{
+				game.AddRoll(0);
+			}
+			game.AddRoll(5);  // 10th frame, first roll
+			game.AddRoll(5);  // 10th frame, second roll
+			game.AddRoll(3);  // 10th frame, third roll
+			Assert.AreEqual(13, game.Frames[9].Score);
+		}
+
+		[Test]
+		public void TestGameEndsAfterTenthFrame()
+		{
+			for (int i = 0; i < 20; i++)
+			{
+				game.AddRoll(1);
+			}
+			game.AddRoll(1); // This should not be added
+			Assert.AreEqual(20, game.Frames[9].Score);
+			Assert.IsNull(game.Frames[9].ThirdRoll);
+		}
+
+		[Test]
+		public void TestStrikeInEighthFrame()
+		{
+			// Roll 7 frames of 1 pin each
+			for (int i = 0; i < 14; i++)
+			{
+				game.AddRoll(1);
+			}
+
+			// Strike in 8th frame
+			game.AddRoll(10);
+
+			// 9th frame
+			game.AddRoll(5);
+			game.AddRoll(3);
+
+			Assert.AreEqual(14, game.Frames[6].Score);
+			Assert.AreEqual(32, game.Frames[7].Score);
+			Assert.AreEqual(40, game.Frames[8].Score);
+		}
+
+		[Test]
+		public void TestSpareInNinthFrame()
+		{
+			// Roll 8 frames of 1 pin each
+			for (int i = 0; i < 16; i++)
+			{
+				game.AddRoll(1);
+			}
+
+			// Spare in 9th frame
+			game.AddRoll(5);
+			game.AddRoll(5);
+
+			// 10th frame
+			game.AddRoll(3);
+			game.AddRoll(2);
+
+			Assert.AreEqual(16, game.Frames[7].Score);
+			Assert.AreEqual(29, game.Frames[8].Score);
+			Assert.AreEqual(34, game.Frames[9].Score);
+		}
+
+		[Test]
+		public void TestStrikeInNinthAndTenthFrames()
+		{
+			// Roll 8 frames of 1 pin each
+			for (int i = 0; i < 16; i++)
+			{
+				game.AddRoll(1);
+			}
+
+			// Strike in 9th frame
+			game.AddRoll(10);
+
+			// Strike in 10th frame
+			game.AddRoll(10);
+			game.AddRoll(5);
+			game.AddRoll(3);
+
+			Assert.AreEqual(16, game.Frames[7].Score);
+			Assert.AreEqual(41, game.Frames[8].Score);
+			Assert.AreEqual(59, game.Frames[9].Score);
+		}
+
+		[Test]
+		public void TestSpareInTenthFrame()
+		{
+			// Roll 9 frames of 1 pin each
+			for (int i = 0; i < 18; i++)
+			{
+				game.AddRoll(1);
+			}
+
+			// Spare in 10th frame
+			game.AddRoll(5);
+			game.AddRoll(5);
+			game.AddRoll(7);
+
+			Assert.AreEqual(18, game.Frames[8].Score);
+			Assert.AreEqual(35, game.Frames[9].Score);
+		}
+
+		[Test]
+		public void TestThreeStrikesInTenthFrame()
+		{
+			// Roll 9 frames of 1 pin each
+			for (int i = 0; i < 18; i++)
+			{
+				game.AddRoll(1);
+			}
+
+			// Three strikes in 10th frame
+			game.AddRoll(10);
+			game.AddRoll(10);
+			game.AddRoll(10);
+
+			Assert.AreEqual(18, game.Frames[8].Score);
+			Assert.AreEqual(48, game.Frames[9].Score);
+		}
+
+		[Test]
+		public void TestGameEndingWithOpenFrameInTenth()
+		{
+			// Roll 9 frames of 1 pin each
+			for (int i = 0; i < 18; i++)
+			{
+				game.AddRoll(1);
+			}
+
+			// Open frame in 10th
+			game.AddRoll(4);
+			game.AddRoll(3);
+
+			Assert.AreEqual(18, game.Frames[8].Score);
+			Assert.AreEqual(25, game.Frames[9].Score);
+			Assert.IsNull(game.Frames[9].ThirdRoll);
+		}
+
+		[Test]
+		public void TestTenthFrameOpenFrameNoThirdRoll()
+		{
+			// Roll 9 frames of 1 pin each
+			for (int i = 0; i < 18; i++)
+			{
+				game.AddRoll(1);
+			}
+
+			// 10th frame - open frame
+			game.AddRoll(4);
+			game.AddRoll(5);
+
+			// Try to add a third roll (this should not be added)
+			game.AddRoll(1);
+
+			Assert.AreEqual(18, game.Frames[8].Score);
+			Assert.AreEqual(27, game.Frames[9].Score);
+			Assert.AreEqual(4, game.Frames[9].FirstRoll);
+			Assert.AreEqual(5, game.Frames[9].SecondRoll);
+			Assert.IsNull(game.Frames[9].ThirdRoll);
+			Assert.AreEqual(9, game.CurrentFrameIndex);
+		}
+	}
+}
---
+++ BowlingScoreKeeper/Components/Pages/Game.cs
@@ -0,0 +1,160 @@
+﻿using NUnit.Framework;
+
+namespace BowlingScoreKeeper.Components.Pages
+{
+	public class Game
+	{
+		public List<GameFrame> Frames { get; set; } = new List<GameFrame>();
+		public int CurrentFrameIndex { get; set; } = 0;
+        public int CurrentGameMaxPossible { get; set; } = 0;
+
+        public event Action OnStateChanged;
+
+        public Game()
+		{
+            for (int i = 0; i < 10; i++)
+            {
+                Frames.Add(new GameFrame()
+                {
+					FrameIndex = i + 1
+				});
+            }
+        }
+
+		public GameFrame CurrentFrame => Frames[CurrentFrameIndex];
+
+        public void AddRoll(int pinsKnocked)
+        {
+            if(CurrentFrameIndex >= 10) 
+            {
+                return;
+            }
+
+			//what is our current frame
+			GameFrame currentFrame = Frames[CurrentFrameIndex];
+
+			//add the pinsKnock to the current frame
+			if (currentFrame.FirstRoll == null)
+            {
+                currentFrame.FirstRoll = pinsKnocked;
+            }
+            else if(currentFrame.SecondRoll == null)
+            {
+                currentFrame.SecondRoll = pinsKnocked;
+            }
+            else if(CurrentFrameIndex == 9 && currentFrame.ThirdRoll == null)
+            {
+                currentFrame.ThirdRoll = pinsKnocked;
+            }
+            else
+            {
+                Assert.Fail("We are trying to add a roll to an out of bounds frame roll");
+            }
+
+            //calculate the score for all frames
+            CalculateScore();
+
+            //CalculateMaximumScore();
+
+			//move frame forward
+            //only move the frame if we got a strike or we are the second roll
+			MoveToNextFrame();
+
+			//invoke statechange update to notify components they need to update there values
+			OnStateChanged?.Invoke();
+		}
+
+        private void MoveToNextFrame()
+        {
+            //If we are not the last frame
+            //And we have fill out the frame
+            if (Frames[CurrentFrameIndex] != Frames.Last() &&
+                (Frames[CurrentFrameIndex].SecondRoll != null ||
+				Frames[CurrentFrameIndex].IsStrike))
+            {
+                CurrentFrameIndex += 1;
+            }
+        }
+
+        private void CalculateScore()
+        {
+            //keep a running total so you can pass that up the next frame
+            int runningTotal = 0;
+
+			//calculate each frame GameScoreUpToCurrentFrame
+			for (int i = 0; i <= CurrentFrameIndex; i++)
+            {
+                GameFrame currentFrame = Frames[i];
+				//get the values for my Frames[i] and set the GameScoreUpToCurrentFrame
+				int currentFramePoints = (currentFrame.FirstRoll ?? 0) + (currentFrame.SecondRoll ?? 0);
+
+				//if you have spares you need to look one roll forward and add that
+				if(currentFrame.IsSpare)
+                {
+                    //you can get an out of bounds if this is the last frame
+                    if(i < 9)
+                    {
+						GameFrame nextFrame = Frames[i + 1];
+						currentFramePoints += nextFrame.FirstRoll ?? 0;
+					}
+                    else
+                    {
+                        //yes you can get here if we get a spare in the 10 frame
+                        //we are in the 10th frame and we just need to add the 3rd shot
+                        currentFramePoints += currentFrame.ThirdRoll ?? 0;
+                    }
+                }
+				//if you have a stike you need to look two rolls forward and add that
+                else if(currentFrame.IsStrike)
+                {
+                    //Frame 8
+                    if (i < 8)
+                    {
+                        //we cannot go out of bounds
+                        GameFrame nextFrame = Frames[i + 1];
+						currentFramePoints += nextFrame.FirstRoll ?? 0;
+						if(nextFrame.IsStrike)
+                        {
+                            GameFrame twoNextFrame = Frames[i + 2];
+                            currentFramePoints += twoNextFrame.FirstRoll ?? 0;
+                        }
+                        else
+                        {
+							currentFramePoints += nextFrame.SecondRoll ?? 0;
+                        }
+
+					}
+                    //Frame 9
+                    else if(i < 9)
+                    {
+						//be carful we can go out of bounds now
+						GameFrame nextFrame = Frames[i + 1];
+                        currentFramePoints += nextFrame.FirstRoll ?? 0;
+                        currentFramePoints += nextFrame.SecondRoll ?? 0;
+					}
+                    //Frame 10
+                    else
+                    {
+                        //we are in the last Frame
+                        currentFramePoints = (currentFrame.FirstRoll ?? 0) + (currentFrame.SecondRoll ?? 0) + (currentFrame.ThirdRoll ?? 0);
+                    }
+				}
+                currentFrame.Score = runningTotal + currentFramePoints;
+                runningTotal = currentFrame.Score ?? 0;
+			}
+		}
+	}
+
+
+	public class GameFrame
+	{
+        public int FrameIndex { get; set; }
+		public int? FirstRoll { get; set; }
+		public int? SecondRoll { get; set; }
+		public int? ThirdRoll { get; set; }
+		public int? Score { get; set; }
+
+		public bool IsStrike => FirstRoll == 10;
+		public bool IsSpare => !IsStrike && (FirstRoll + SecondRoll == 10);
+	}
+}
